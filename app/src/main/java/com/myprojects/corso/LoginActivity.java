bf189c7ef5655f7846be9552babe88ce5df96685
Domide Nichita <nichitadomide@gmail.com>
2019-10-20 23:43:58 +0300

    Added Facebook Login

--- app/src/main/java/com/myprojects/corso/LoginActivity.java
+++ app/src/main/java/com/myprojects/corso/LoginActivity.java
@@ -10,14 +10,31 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -29,6 +46,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private FirebaseFirestore db;
     private boolean check;
     private boolean access;
+    private CallbackManager mCallbackManager;
+    private boolean isFacebookLogin = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,10 +95,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                                     Toast.makeText(LoginActivity.this, "Email adress is not verified",
                                             Toast.LENGTH_SHORT).show();
                                 }
-                            } catch(Exception e){}
-
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            } catch(Exception e) {
+                                Log.d("why", e.getMessage());
+                                Toast.makeText(LoginActivity.this, "Authentication failed.",
                                         Toast.LENGTH_SHORT).show();
+                                updateUI(null);
+                            }
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
                             updateUI(null);
                         }
                         hideProgressDialog();
@@ -114,6 +137,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void updateUI(FirebaseUser user) {
+        Log.d(TAG, "called on user " + user + " access is :" + access + " email is vreified :" + user.isEmailVerified());
         hideProgressDialog();
 
         if (user != null) {
@@ -128,7 +152,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             Toast.LENGTH_SHORT).show();
                 }
             }
-            else if(user.isEmailVerified()) {
+            else if(user.isEmailVerified() || isFacebookLogin) {
                 Log.d(TAG, "updateUI: moved to else if");
                 Intent myIntent = new Intent(LoginActivity.this, MenuActivity.class);
                 LoginActivity.this.startActivity(myIntent);
@@ -145,6 +169,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
         if (i == R.id.emailSignInButton) {
             signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        }
+        if (i == R.id.facebook){
+            logWithFb();
         }
     }
 
@@ -191,6 +218,109 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 } else {
                     Log.d(TAG, "get failed with ", task.getException());
                     return;
+                }
+            }
+        });
+    }
+    private void logWithFb(){
+        LoginButton loginButton = findViewById(R.id.facebook_fake);
+        loginButton.performClick();
+        mCallbackManager = CallbackManager.Factory.create();
+        loginButton.setReadPermissions("email","public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                Toast.makeText(LoginActivity.this, "Facebook Login didn't work",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                Toast.makeText(LoginActivity.this, "Facebook Login didn't work",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        showProgressDialog();
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            isFacebookLogin = true;
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Log.d(TAG, "after sign in users is :" + user);
+                            addToDatabase(user);
+                        } else {
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                        hideProgressDialog();
+                    }
+                });
+    }
+
+    private void addToDatabase (FirebaseUser user) {
+        Log.d(TAG, "add to data base" + user);
+        DocumentReference docIdRef = db.collection("users").document(user.getEmail());
+        docIdRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "Document exists!");
+                    } else {
+                        Map<String, Object> tracker = new HashMap<>();
+                        tracker.put("Monday", 0);
+                        tracker.put("Tuesday", 0);
+                        tracker.put("Wednesday", 0);
+                        tracker.put("Thursday", 0);
+                        tracker.put("Friday", 0);
+                        tracker.put("Saturday", 0);
+                        tracker.put("Sunday", 0);
+                        tracker.put("today", 0);
+                        tracker.put("coffee_shop", false);
+                        tracker.put("access", true);
+                        db.collection("users").document(user.getEmail())
+                                .set(tracker)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error writing document", e);
+                                    }
+                                });
+                        isCoffeeShop();
+                    }
+                } else {
+                    Log.d(TAG, "Failed with: ", task.getException());
                 }
             }
         });
